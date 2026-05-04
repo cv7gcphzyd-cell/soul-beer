088405b81afee185a7d278ca56cb3a61a8838ad3
cv7gcphzyd-cell <cv7gcphzyd@privaterelay.appleid.com>
2026-05-04 20:34:28 +0530

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        beer: '#D4AF37',
+        darkbeer: '#2C1A0F',
+      }
+    },
+  },
+}
